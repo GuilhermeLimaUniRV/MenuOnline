0978f924fbae6c31a6f31066a7aa1fe56c288bd2
Guiml345 <guiml3456543@gmail.com>
2025-05-03 20:23:00 -0300

    use rotras

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,13 +1,18 @@
-// server.js
 import express from 'express';
-import menuRoutes from './routes/menuRoutes.js'; // .js obrigatório
-import supabase from './config/db.js'; // Importe o Supabase
+import pratoRoutes from './routes/pratoRoutes.js';
+import categoriaRoutes from './routes/categoriaRoutes.js';
+import pedidoRoutes from './routes/pedidoRoutes.js';
+import 'dotenv/config';
 
 const app = express();
 app.use(express.json());
 
+// Rotas
+app.use('/api/pratos', pratoRoutes);
+app.use('/api/categorias', categoriaRoutes);
+app.use('/api/pedidos', pedidoRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
