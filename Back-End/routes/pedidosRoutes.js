51e97fa36f4abb32505c4e758573199f8820f85e
Guiml345 <guiml3456543@gmail.com>
2025-05-03 20:51:24 -0300

    Revert "Merge pull request #25 from GuilhermeLimaUniRV/refactor/server.js"
    This reverts commit b0e8f360c1e4f595df2a2de0ded4c1473b43ee9f, reversing
    changes made to 2b33cca51ea44888456bda9a64f1ac3ab3a59e18.

--- Back-End/routes/pedidosRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { criarPedido } from '../controllers/pedidoController.js';
-
-const router = express.Router();
-
-router.post('/', criarPedido);
-
-export default router;
