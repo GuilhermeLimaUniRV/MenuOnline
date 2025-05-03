681a81a857e9b4a405c2d39d08712b91b51d3735
Guiml345 <guiml3456543@gmail.com>
2025-05-03 20:15:19 -0300

    remoção rota "menu"

--- Back-End/server.js
+++ Back-End/server.js
@@ -6,7 +6,6 @@ import supabase from './config/db.js'; // Importe o Supabase
 const app = express();
 app.use(express.json());
 
-app.use('/api/menu', menuRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
