c8d53c3a39c30c30e0cc2c4a0477ef61b107fc4c
Guiml345 <guiml3456543@gmail.com>
2025-05-06 19:46:16 -0300

    fix App.jsx

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';  // Ajuste o caminho conforme necessário
 import Cardapio from './Pages/Cardapio';  // Importe o componente Cardapio
@@ -13,4 +12,6 @@ function App() {
       </Routes>
     </Router>
   );
+}
+
 export default App;
